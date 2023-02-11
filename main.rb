def8fc78c70daf9823139be2d626e3af478af188
Toru Kurahashi <toru_kurahashi@eastback.jp>
2023-02-11 11:15:56 +0900

    敵を作成、ファイルを分ける

--- main.rb
+++ main.rb
@@ -1,19 +1,24 @@
 # 第１回Ruby合宿勉強会用
 require 'dxruby'
 
-class Player < Sprite
-    def update
-      self.x += Input.x
-      self.y += Input.y
-    end
-end
-  
+require_relative 'player'
+require_relative 'enemy'
+
 player_img = Image.load("image/player.png")
 x = 100
 y = 100
 player = Player.new(x, y, player_img)
 
+enemy_img = Image.load("image/enemy.png")
+x = 300
+y = 300
+enemy = Enemy.new(x, y, enemy_img)
+
 Window.loop do
-player.update
-player.draw
+  player.update
+  player.draw
+
+  enemy.draw
+
+  Sprite.check(player, enemy)
 end
