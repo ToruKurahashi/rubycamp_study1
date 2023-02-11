4a832b6c61963d84cbc86954c5a4bdbfd00d91dd
Toru Kurahashi <toru_kurahashi@eastback.jp>
2023-02-11 11:07:58 +0900

    プレイヤーのクラス化

--- main.rb
+++ main.rb
@@ -1,12 +1,19 @@
 # 第１回Ruby合宿勉強会用
-require "DXRuby"
+require 'dxruby'
 
+class Player < Sprite
+    def update
+      self.x += Input.x
+      self.y += Input.y
+    end
+end
+  
 player_img = Image.load("image/player.png")
 x = 100
 y = 100
+player = Player.new(x, y, player_img)
 
 Window.loop do
-  x = x + Input.x
-  y = y + Input.y
-  Window.draw(x, y, player_img)
+player.update
+player.draw
 end
